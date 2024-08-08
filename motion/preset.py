d2c29746b4fa5e055684a30a9a39b7c9b9fb1d3b
hrim <hyeongrae.ihm@voyagerx.com>
2024-08-08 13:23:58 +0900

    modify head to node for future extension

--- motion/preset.py
+++ motion/preset.py
@@ -17,27 +17,27 @@ def initialize(
 ):
     node_indices = torch.arange(num_node).view(*node_shape)
 
-    heads = initialize_heads(node_shape, zero_index, off_std)
-    edges = initialize_edges(heads, connection_limit)
-    edge_trajs = initialize_edge_trajectories(edges, heads, sample_per_trajectory)
+    nodes = initialize_nodes(node_shape, zero_index, off_std)
+    edges = initialize_edges(nodes, connection_limit)
+    edge_trajs = initialize_edge_trajectories(edges, nodes, sample_per_trajectory)
     breakpoint()
 
 
 
-def initialize_heads(node_shape: "tuple[int, int]", zero_index: "tuple[int, int]", off_std: float) -> torch.Tensor:
-    heads = []
+def initialize_nodes(node_shape: "tuple[int, int]", zero_index: "tuple[int, int]", off_std: float) -> torch.Tensor:
+    nodes = []
     for i in range(node_shape[0]):
         for j in range(node_shape[1]):
-            head_ij = torch.tensor([i-zero_index[0], j-zero_index[1]], dtype=torch.float)
-            head_ij += torch.zeros_like(head_ij).normal_(0, off_std)
-            heads.append(head_ij)
-    heads = torch.stack(heads)
+            node_ij = torch.tensor([i-zero_index[0], j-zero_index[1]], dtype=torch.float)
+            node_ij += torch.zeros_like(node_ij).normal_(0, off_std)
+            nodes.append(node_ij)
+    nodes = torch.stack(nodes)
 
-    return heads
+    return nodes
 
-def initialize_edges(heads, connection_limit, num_trial=100):
+def initialize_edges(nodes, connection_limit, num_trial=100):
     i = 0
-    while invalid_edges(edges:=get_edges(heads, connection_limit)):
+    while invalid_edges(edges:=get_edges(nodes, connection_limit)):
         i+=1
         if i >= num_trial:
             raise ValueError("there are separate subgraphs or no-edge nodes. Larger connection_limit needed")
@@ -70,24 +70,24 @@ def has_unreachable(edges):
                 return True
     return False
 
-def get_edges(heads, connection_limit):
-    distances = torch.zeros((len(heads), len(heads)))
-    for i in range(len(heads)):
-        for j in range(i+1, len(heads)):
-            dist_ij = torch.linalg.norm(heads[i]-heads[j], 2)
+def get_edges(nodes, connection_limit):
+    distances = torch.zeros((len(nodes), len(nodes)))
+    for i in range(len(nodes)):
+        for j in range(i+1, len(nodes)):
+            dist_ij = torch.linalg.norm(nodes[i]-nodes[j], 2)
             distances[i, j] = dist_ij
             distances[j, i] = dist_ij
     edges = distances.lt(connection_limit)
-    for i in range(len(heads)):
+    for i in range(len(nodes)):
         edges[i, i] = False
     return edges
 
-def initialize_edge_trajectories(edges, heads, sample_per_trajectory):
+def initialize_edge_trajectories(edges, nodes, sample_per_trajectory):
     edge_trajectory = torch.zeros([*edges.shape, sample_per_trajectory])
     for i in range(edges.shape[0]):
         for j in range(i+1, edges.shape[0]):
             if edges[i, j]:
-                traj = smooth(heads[i][0], heads[j][0], sample_per_trajectory + 2)[1:-1]
+                traj = smooth(nodes[i][0], nodes[j][0], sample_per_trajectory + 2)[1:-1]
                 # + 2 and trimming both edge because smooth result is closed range
                 edge_trajectory[i, j] = traj
                 edge_trajectory[j, i] = torch.flip(traj, (0,))
